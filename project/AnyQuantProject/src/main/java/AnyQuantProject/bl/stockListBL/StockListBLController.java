e177b90dba7dd9567a57bbf3db614cf01c66f13d
cx14 <cx14@software.nju.edu.cn>
2016-03-08 22:13:36 +0800

    search

--- project/AnyQuantProject/src/main/java/AnyQuantProject/bl/stockListBL/StockListBLController.java
+++ project/AnyQuantProject/src/main/java/AnyQuantProject/bl/stockListBL/StockListBLController.java
@@ -1,5 +1,6 @@
 package AnyQuantProject.bl.stockListBL;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +14,7 @@ import AnyQuantProject.dataService.realDATAService.stockListDATAService.StockLis
 import AnyQuantProject.dataStructure.Exchange;
 import AnyQuantProject.dataStructure.Stock;
 import AnyQuantProject.util.method.CalendarHelper;
+import AnyQuantProject.util.method.Checker;
 
 /** 
 *AnyQuantProject//AnyQuantProject.bl.stockListBL//StockListBLController.java
@@ -21,33 +23,54 @@ import AnyQuantProject.util.method.CalendarHelper;
 */
 
 public class StockListBLController implements StockListBLService {
+	private List<String> avaliable;
+	public StockListBLController() {
+		avaliable=new LinkedList<>();
+		// get dataService
+		FactoryDATAService factoryDATAService = FactoryDATA.getInstance();
+		StockListDATAService stockListDATAService = factoryDATAService.getStockListDATAService();
+		// get names
+		List<String> sz = stockListDATAService.getAllStocks(Calendar.getInstance(), Exchange.SZ);
+		List<String> sh = stockListDATAService.getAllStocks(Calendar.getInstance(), Exchange.SH);
+		avaliable.addAll(sh);
+		avaliable.addAll(sz);
+	}
 
 	@Override
 	public List<Stock> getAllStocks() {
-		//get dataService
-		FactoryDATAService factoryDATAService=FactoryDATA.getInstance();
-		StockListDATAService stockListDATAService=factoryDATAService.getStockListDATAService();
+		FactoryDATAService factoryDATAService = FactoryDATA.getInstance();
 		SingleStockDATAService singleStockDATAService=factoryDATAService.getSingleStockDATAService();
-		//get names
-		List<String> avaliable=new LinkedList<>();
-		//TODO
-//		List<String> sz=stockListDATAService.getAllStocks(Calendar.getInstance(), Exchange.SZ);
-		List<String> sh=stockListDATAService.getAllStocks(Calendar.getInstance(), Exchange.SH);
-		avaliable.addAll(sh);
-//		avaliable.addAll(sz);
-		
-		//TODO
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.YEAR, 2015);
-		
-		
 		//get data
 		List<Stock> ans=avaliable.stream()
 				.map(name -> 
-//				singleStockDATAService.getOperation(name, CalendarHelper.getPreviousDay(Calendar.getInstance())))
 				singleStockDATAService.getOperation(name, CalendarHelper.getPreviousDay(c)))
 				.collect(Collectors.toList());
 		return ans;
+	}
+
+	@Override
+	public List<String> searchPredict(String target) {
+		if (Checker.checkStringNotNull(target)) {
+			return new ArrayList<>();
+		}
+		// get dataService
+		FactoryDATAService factoryDATAService = FactoryDATA.getInstance();
+		StockListDATAService stockListDATAService = factoryDATAService.getStockListDATAService();
+		SingleStockDATAService singleStockDATAService = factoryDATAService.getSingleStockDATAService();
+		// get names
+		List<String> ans=avaliable
+				.stream()
+				.filter(nam->nam.contains(target))
+				.collect(Collectors.toList());
+		return ans;
+		
+	}
+
+	@Override
+	public boolean searchLegal(String target) {
+		return avaliable.contains(target);
 	}
 
 }
