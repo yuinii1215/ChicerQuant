633f165f7d4dea8c32990eb32d25b6ca23efdfb2
hmy14 <hmy14@software.nju.edu.cn>
2016-04-05 00:15:24 +0800

    changes on all stocks ui

--- project/AnyQuantProject/src/main/java/AnyQuantProject/ui/allStocksUI/AllStocksUIController.java
+++ project/AnyQuantProject/src/main/java/AnyQuantProject/ui/allStocksUI/AllStocksUIController.java
@@ -147,19 +147,24 @@ public class AllStocksUIController implements Initializable {
 
     private static AllStocksUIController instance;
     StockListBLService stockListImplement = StockListBLFactory.getStockListBLService();
-    CalculateLineBLService calculateLineImplement=LineChartBLFactory.getCalculateLineBL();
+    CalculateLineBLService calculateLineImplement = LineChartBLFactory.getCalculateLineBL();
 
     private XYChart.Series<Number, Number> hourDataSeries;
     private XYChart.Series<Number, Number> minuteDataSeries;
-    private NumberAxis xAxis;
+    private CategoryAxis xAxis;
+    private NumberAxis yAxis;
     private Timeline animation;
 
     private double hours = 0;
     private double minutes = 0;
     private double timeInHours = 0;
     private double prevY = 10;
+
     private double y = 10;
     LineChartData previewLineChart;
+
+    ;
+
 
     public static AllStocksUIController getInstance() {
         System.out.println("here is the instance of AllStocksUIController ");
@@ -241,143 +246,24 @@ public class AllStocksUIController implements Initializable {
         pbColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(
                 cellData.getValue().getPb()));
 
-        
-       // final CategoryAxis xAxis = new CategoryAxis();
-       //final NumberAxis yAxis = new NumberAxis();
-        previewLineChart=LineChartBLFactory.getCalculateLineBL().drawPreview(stockName);     
-        
-        
-        stockLineChart = (LineChart<String, Number>) new LineChart<>(previewLineChart.getxAxis(),previewLineChart.getyAxis());
+        previewLineChart = LineChartBLFactory.getCalculateLineBL().drawPreview(stockName);
+        xAxis = new CategoryAxis();
+        xAxis = (CategoryAxis) previewLineChart.getxAxis();
+        yAxis = new NumberAxis();
+        yAxis = (NumberAxis) previewLineChart.getyAxis();
+        yAxis.setLowerBound(yAxis.getLowerBound() - 1);
+        yAxis.setUpperBound(yAxis.getUpperBound() + 1);
+        stockLineChart = (LineChart<String, Number>) new LineChart<>(xAxis, yAxis);
         stockLineChart.setTitle(previewLineChart.getTitle());
-        
-//        XYChart.Series series1=new XYChart.Series();
-        
-        
 
-        stockLineChart.getData().add((XYChart.Series<String, Number>)previewLineChart.getSeries().get(0));
-//        stockLineChart.setLayoutX(0);
-//        stockLineChart.setLayoutY(0);
-        stockLineChart.setPrefSize(205, 210);
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
 
+        // series=(XYChart.Series<String, Number>)previewLineChart.getSeries();
+        stockLineChart.getData().add((XYChart.Series<String, Number>) previewLineChart.getSeries().get(0));
+
+        stockLineChart.setPrefSize(200, 200);
         chartPane.getChildren().add(stockLineChart);
-       
-//        animation = new Timeline();
-//        animation.getKeyFrames().add(new KeyFrame(Duration.millis(1000 / 60), new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                for (int count = 0; count < 6; count++) {
-//                    nextTime();
-//                    plotTime();
-//                }
-//            }
-//        }));
-//        animation.setCycleCount(Animation.INDEFINITE);
-//        animation.setCycleCount(100);
-//        chartPane.getChildren().add(createChart());
-//        play();
-    }
 
-    /**
-     * Copyright (c) 2008, 2012 Oracle and/or its affiliates. All rights
-     * reserved. Use is subject to license terms.
-     */
-    /**
-     * A simulated stock line chart.
-     *
-     * @see javafx.scene.chart.Chart
-     * @see javafx.scene.chart.LineChart
-     * @see javafx.scene.chart.NumberAxis
-     * @see javafx.scene.chart.XYChart
-     */
-    protected LineChart<Number, Number> createChart() {
-        xAxis = new NumberAxis(0, 24, 3);
-        final NumberAxis yAxis = new NumberAxis(0, 100, 10);
-        final LineChart<Number, Number> lc = new LineChart<Number, Number>(xAxis, yAxis);
-        // setup chart
-        lc.setId("lineStockDemo");
-        lc.setCreateSymbols(false);
-        lc.setAnimated(false);
-        lc.setLegendVisible(false);
-        lc.setTitle("ACME Company Stock");
-        xAxis.setLabel("Time");
-        xAxis.setForceZeroInRange(false);
-        yAxis.setLabel("Share Price");
-        yAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(yAxis, "$", null));
-        // add starting data
-        hourDataSeries = new XYChart.Series<Number, Number>();
-        hourDataSeries.setName("Hourly Data");
-        minuteDataSeries = new XYChart.Series<Number, Number>();
-        minuteDataSeries.setName("Minute Data");
-        // create some starting data
-        hourDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, prevY));
-        minuteDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, prevY));
-        for (double m = 0; m < (60); m++) {
-            nextTime();
-            plotTime();
-        }
-        lc.getData().add(minuteDataSeries);
-        lc.getData().add(hourDataSeries);
-        lc.setPrefSize(200, 200);
-        return lc;
-    }
-
-    private void nextTime() {
-        if (minutes == 59) {
-            hours++;
-            minutes = 0;
-        } else {
-            minutes++;
-        }
-        timeInHours = hours + ((1d / 60d) * minutes);
-    }
-
-    private void plotTime() {
-        if ((timeInHours % 1) == 0) {
-            // change of hour
-            double oldY = y;
-            y = prevY - 10 + (Math.random() * 20);
-            prevY = oldY;
-            while (y < 10 || y > 90) {
-                y = y - 10 + (Math.random() * 20);
-            }
-            hourDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, prevY));
-            // after 25hours delete old data
-            if (timeInHours > 25) {
-                hourDataSeries.getData().remove(0);
-            }
-            // every hour after 24 move range 1 hour
-            if (timeInHours > 24) {
-                xAxis.setLowerBound(xAxis.getLowerBound() + 1);
-                xAxis.setUpperBound(xAxis.getUpperBound() + 1);
-            }
-        }
-        double min = (timeInHours % 1);
-        double randomPickVariance = Math.random();
-        if (randomPickVariance < 0.3) {
-            double minY = prevY + ((y - prevY) * min) - 4 + (Math.random() * 8);
-            minuteDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, minY));
-        } else if (randomPickVariance < 0.7) {
-            double minY = prevY + ((y - prevY) * min) - 6 + (Math.random() * 12);
-            minuteDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, minY));
-        } else if (randomPickVariance < 0.95) {
-            double minY = prevY + ((y - prevY) * min) - 10 + (Math.random() * 20);
-            minuteDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, minY));
-        } else {
-            double minY = prevY + ((y - prevY) * min) - 15 + (Math.random() * 30);
-            minuteDataSeries.getData().add(new XYChart.Data<Number, Number>(timeInHours, minY));
-        }
-        // after 25hours delete old data
-        if (timeInHours > 25) {
-            minuteDataSeries.getData().remove(0);
-        }
-    }
-
-    public void play() {
-        animation.play();
-    }
-
-    public void stop() {
-        animation.pause();
     }
 
     public class TableRowControl<T> extends TableRow<T> {
@@ -406,17 +292,21 @@ public class AllStocksUIController implements Initializable {
                         StockInfo2Column.setKValue(key_column2);
                         StockInfo2Column.setVValue(value_column2);
 
-//                        dayChart = drawDayKLine();
-//                        panel1 = getChartPanel(dayChart);
-//                        swingNode1.setContent(panel1);
-//
-//                        
-//                        scroller1.setContent(swingNode1);
-//                        scroller1.setFitToHeight(true);
-//                        scroller1.setMaxSize(250, 230);
-//                        scroller1.setHvalue(1);
-//                        chartPane.getChildren().clear();
-//                        chartPane.getChildren().add(scroller1);
+                        previewLineChart = LineChartBLFactory.getCalculateLineBL().drawPreview(stockName);
+                        xAxis = (CategoryAxis) previewLineChart.getxAxis();
+                        yAxis = (NumberAxis) previewLineChart.getyAxis();
+                        yAxis.setLowerBound(yAxis.getLowerBound() - 1);
+                        yAxis.setUpperBound(yAxis.getUpperBound() + 1);
+                        stockLineChart = (LineChart<String, Number>) new LineChart<>(xAxis, yAxis);
+                        stockLineChart.setTitle(previewLineChart.getTitle());
+
+//                        XYChart.Series<String, Number> series = new XYChart.Series<>();
+                        // series=(XYChart.Series<String, Number>)previewLineChart.getSeries();
+                        
+                        stockLineChart.getData().add((XYChart.Series<String, Number>) previewLineChart.getSeries().get(0));
+                        stockLineChart.setPrefSize(200, 200);
+                        chartPane.getChildren().clear();
+                        chartPane.getChildren().add(stockLineChart);
                     }
                     if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
                         selectedIndex = TableRowControl.this.getIndex();
