3ec4ea9420551b4c345f3b7f87414d82e0703d99
cx14 <141250018@smail.nju.edu.cn>
2016-04-29 10:46:16 +0800

    vertx

---
+++ project/AnyQuantProject/src/main/java/webProject/server/myHandler/HtmlHander.java
@@ -0,0 +1,40 @@
+package webProject.server.myHandler;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.mchange.io.FileUtils;
+
+import io.vertx.core.Handler;
+import io.vertx.ext.web.RoutingContext;
+import webProject.resources.Resources;
+
+/**
+* AnyQuantProject/webProject.server.myHandler/HtmlHander.java
+* @author cxworks
+* 2016年4月29日 上午8:25:12
+*/
+public class HtmlHander implements Handler<RoutingContext>{
+//	public static void main(String[] args) {
+//		System.out.println(Resources.class.getResource("index.html").getPath());
+//		System.out.println(new File(Resources.class.getResource("index.html").getPath()).exists());
+//	}
+	
+
+	@Override
+	public void handle(RoutingContext event) {
+		
+		String index=event.request().getParam("type");
+		File file=new File(Resources.class.getResource(index+".html").getPath());
+		try {
+			String html=FileUtils.getContentsAsString(file);
+			event.response().setChunked(true);
+			event.response().putHeader("content-type", "text/html").write(html).end();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ project/AnyQuantProject/src/main/java/webProject/server/myVerticle/RouterVerticle.java
@@ -0,0 +1,27 @@
+package webProject.server.myVerticle;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import webProject.server.myHandler.HtmlHander;
+
+/**
+* AnyQuantProject/webProject.server/RouterVerticle.java
+* @author cxworks
+* 2016年4月29日 上午8:16:56
+*/
+public class RouterVerticle extends AbstractVerticle {
+
+	@Override
+	public void start(){
+		HttpServer httpServer=vertx.createHttpServer();
+		Router homeRouter=Router.router(vertx);
+		homeRouter.route(HttpMethod.GET,"/:type").handler(new HtmlHander());
+		
+//		homeRouter.route("*").handler()
+		
+		httpServer.requestHandler(homeRouter::accept).listen(8080);
+		
+	}
+}
