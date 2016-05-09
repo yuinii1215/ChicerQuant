c6332b3cb6182c60ffa9d69ffc0878197389730c
hq14 <hq14@software.nju.edu.cn>
2016-05-09 19:02:22 +0800

    button modify

--- project/AnyQuantProject/src/main/java/webProject/resources/js/button.js
+++ project/AnyQuantProject/src/main/java/webProject/resources/js/button.js
@@ -2,6 +2,6 @@ var app = angular.module("myApp", []);
 app.controller("enterHome", function($scope) {
 	$scope.hi = "122";
     $scope.enter = function() {
-        $scope.location="../homePage/home.html";
+        $scope.go="../homePage/home.html";
     };
 });
