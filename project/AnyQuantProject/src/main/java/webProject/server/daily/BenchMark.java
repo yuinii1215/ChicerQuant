59990e1377e9c1cb5f2304af47fc77681bd1a949
cx14 <141250018@smail.nju.edu.cn>
2016-05-12 08:17:22 +0800

    sabaobao

--- project/AnyQuantProject/src/main/java/webProject/server/daily/BenchMark.java
+++ project/AnyQuantProject/src/main/java/webProject/server/daily/BenchMark.java
@@ -5,26 +5,9 @@ package webProject.server.daily;
 * 2016年5月5日 下午7:20:55
 */
 public class BenchMark extends Common {
-	int transection;
-	int marketvalue;
-	int flow;
-	public int getTransection() {
-		return transection;
-	}
-	public void setTransection(int transection) {
-		this.transection = transection;
-	}
-	public int getMarketvalue() {
-		return marketvalue;
-	}
-	public void setMarketvalue(int marketvalue) {
-		this.marketvalue = marketvalue;
-	}
-	public int getFlow() {
-		return flow;
-	}
-	public void setFlow(int flow) {
-		this.flow = flow;
-	}
+	long transaction;
+	long marketvalue;
+	long flow;
+	
 	
 }
