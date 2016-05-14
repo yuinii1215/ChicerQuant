cd27b969732a3411508d62b6896a7fcc6e2c54aa
hq14 <hq14@software.nju.edu.cn>
2016-05-14 23:47:02 +0800

    search

---
+++ project/AnyQuantProject/src/main/java/webProject/resources/homePage/js/search.js
@@ -0,0 +1,8 @@
+/**
+ * Created by QiHan on 2016/5/14.
+ */
+function toSingleStockPage() {
+    var parm1=document.getElementById("stockID").value;
+    localStorage.singleStockID=parm1;
+    window.location.href="../singleStockPage/singleStockPage.html";
+}
