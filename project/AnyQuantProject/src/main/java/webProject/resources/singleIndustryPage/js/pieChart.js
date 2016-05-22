0df1a3cef308aa4211e71235cb43ec24e39f2e54
hq14 <hq14@software.nju.edu.cn>
2016-05-22 13:27:46 +0800

    single content finished

--- project/AnyQuantProject/src/main/java/webProject/resources/singleIndustryPage/js/pieChart.js
+++ project/AnyQuantProject/src/main/java/webProject/resources/singleIndustryPage/js/pieChart.js
@@ -21,8 +21,17 @@ require(
 
         setTimeout(function() {
             var pieChart1 = ec.init(document.getElementById('pieChart1'));
+            var pieChart2 = ec.init(document.getElementById('pieChart2'));
 
             pieChart1.showLoading({
+                itemStyle: {normal: {color:'#ffffff', label:{show:true}}},
+                text: '正在努力加载中...',
+                textStyle: {
+                    fontSize: 10,
+                }
+            });
+
+            pieChart2.showLoading({
                 itemStyle: {normal: {color:'#ffffff', label:{show:true}}},
                 text: '正在努力加载中...',
                 textStyle: {
@@ -33,7 +42,12 @@ require(
             $.ajaxSettings.async = false;
 
             var num= $scope.singleNum;
-            console.log("num:"+parseInt(num));
+            var up= $scope.Up;
+            var down=$scope.Down;
+            var keep=$scope.Keep;
+
+         //   console.log("num:"+parseInt(num));
+
             var option = {
                 tooltip: {
                     trigger: 'item',
@@ -77,16 +91,80 @@ require(
                 ]
             };
 
+            var option2 = {
+                tooltip: {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b}: {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    x: 'top',
+                    data:['上涨','下跌','持平']
+                },
+                series: [
+                    {
+                        name:'占比',
+                        type:'pie',
+                        radius: ['50%', '70%'],
+                        avoidLabelOverlap: false,
+                        label: {
+                            normal: {
+                                show: false,
+                                position: 'center'
+                            },
+                            emphasis: {
+                                show: true,
+                                textStyle: {
+                                    fontSize: '30',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
+                        labelLine: {
+                            normal: {
+                                show: false
+                            }
+                        },
+                        data:[
+                            {value:parseInt(up), name:"上涨"},
+                            {value:parseInt(down), name:'下跌'},
+                            {value:parseInt(keep), name:'持平'},
+
+                        ]
+                    }
+                ]
+            };
+
             pieChart1.setOption(option);
             pieChart1.hideLoading();
+            pieChart2.setOption(option2);
+            pieChart2.hideLoading();
 
             setTimeout(function () {
                 window.onresize = function () {
                     pieChart1.resize();
+                    pieChart2.resize();
                 }
             }, 100);
-
 
         },1500);
 
     });
+
+
+/*      var  colors=['#f6909e','#83dce7'];
+ var i=0;
+ itemStyle : {
+ normal : {
+ label: {
+ show: false
+ },
+ labelLine: {
+ show: false
+ },
+ //自定义颜色数组
+ color: function () {
+ return colors[i++];
+ }
+ }
+ },*/
