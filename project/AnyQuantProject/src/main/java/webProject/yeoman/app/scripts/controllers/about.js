c0a354330b1486a45234a4bd5e67f145eecf3cbf
cx14 <141250018@smail.nju.edu.cn>
2016-05-22 23:29:16 +0800

    yeo and k

---
+++ project/AnyQuantProject/src/main/java/webProject/yeoman/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanApp
+ */
+angular.module('yeomanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
