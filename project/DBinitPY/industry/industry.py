5135e93c21fdb78310a133fd079eb1419ddcbbce
cx14 <141250018@smail.nju.edu.cn>
2016-07-13 00:28:21 +0800

    industry init

--- project/DBinitPY/industry/industry.py
+++ project/DBinitPY/industry/industry.py
@@ -4,7 +4,10 @@ import datetime
 import pandas as pd
 import numpy as np
 from AnyQuant import getSingleStock
+from pdCal import updown
 
+def apply_lambda(df):
+    return df.sort_values(by='updown')[-1:]
 
 
 def transTable(id):
@@ -32,12 +35,33 @@ def oneIndustry(industry,i_c):
     starttime=datetime.datetime(2016,1,1)
     endtime=datetime.datetime.now()
     use=[]
-#     for id in i_c.index:
-    te=getSingleStock(transTable(600051),starttime,endtime)
-    te['stock_id']=600051
-    resu=pd.merge(left=te,right=set[['stock_id',]],on='stock_id')
-    print resu
-        
+    count=0
+    for id in i_c.index:
+        te=getSingleStock(transTable(id),starttime,endtime)
+        updown(te)
+        te.reset_index(inplace=True)
+        te['stock_id']=id
+        resu=pd.merge(left=te[['stock_id','open','high','close','low','volume','updown','date']],right=set[['stock_id','stock_name','percent']],on='stock_id')
+        resu['open']=resu['open']*resu['percent']
+        resu['realclose']=resu['close']
+        resu['close']=resu['close']*resu['percent']
+        resu['high']=resu['high']*resu['percent']
+        resu['low']=resu['low']*resu['percent']
+        resu['volume']=resu['volume']*resu['percent']
+        use.append(resu)
+        count+=1
+        if count>1:
+            break
+    data=use[0]
+    for i in range(1,len(use)):
+        data=data.append(use[i])
+    data.fillna(0,inplace=True)
+    df1=data[['open','high','low','close','volume']].groupby(data['date']).sum()
+    df1.reset_index(level=0, inplace=True)
+#     print df1
+    df2=data[['stock_name','updown','stock_id','date','realclose']].groupby(data['date'],as_index=False).apply(apply_lambda)
+    df=pd.merge(left=df1,right=df2,on='date')
+    print df
     return
     
     
