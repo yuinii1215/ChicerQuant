8c09227a705724dddd0886fef0a190d778b7ff5c
cx14 <141250018@smail.nju.edu.cn>
2016-09-02 22:34:32 +0800

    bug notes

--- project/DBinitPY/Daily.py
+++ project/DBinitPY/Daily.py
@@ -13,7 +13,7 @@ def daily(now):
     for v in list:
         print str(count)+'/'+str(total),'start',v
         try:
-            df=oneStock(v,starttime=before)
+            df=oneStock(v,starttime=before,endtime=now)
             df=df.iloc[[-1]]
             [conn,cur]=getCursor()
             pdsql.to_sql(df, v, conn, flavor='mysql',  if_exists='append',index_label='date')
@@ -25,6 +25,21 @@ def daily(now):
         count+=1
     return
 
+def test(now):
+    import datetime
+    from sql import getCursor,insertStock
+    from stock import oneStock,getStockList
+    import pandas.io.sql as pdsql
+    before=now-datetime.timedelta(700)
+    v='sh600216'
+    df=oneStock(v,starttime=before,endtime=time)
+    df=df.iloc[[-1]]
+    [conn,cur]=getCursor()
+    pdsql.to_sql(df, v, conn, flavor='mysql',  if_exists='append',index_label='date')
+            
+        
+    return
+
 if __name__ == '__main__':
     import sys
     import tushare as ts
@@ -34,7 +49,8 @@ if __name__ == '__main__':
         time=datetime.strptime(t[1],'%Y-%m-%d')
     else:
         time=datetime.now()
-        print datetime.strftime(time,format='%Y-%m-%d')
+        
+    print datetime.strftime(time,format='%Y-%m-%d')
     if ts.is_holiday(date=datetime.strftime(time,format='%Y-%m-%d')) :
         print 'is holiday'
     else:
