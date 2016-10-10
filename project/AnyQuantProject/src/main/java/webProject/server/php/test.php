ae6e5b701ff01211c6c675651e11a386642a57df
ctg14 <ctg14@software.nju.edu.cn>
2016-10-10 21:38:19 +0800

    test

---
+++ project/AnyQuantProject/src/main/java/webProject/server/php/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: G
+ * Date: 2016/10/10
+ * Time: 下午9:34
+ */
+    $cmd = "rqalpha run -f examples/28.py -o 28.pkl -s 2016-07-01 -e 2016-09-30 --no-plot";
+     system($cmd);
+     $cmd2 = "python pickle2json.py 28.pkl";
+     system($cmd2,$ret);
+     echo $ret
+
+?>
