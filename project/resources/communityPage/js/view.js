c97504c0755f50fba1cc501950bafbb7d767cfce
Grace <hmybbok@126.com>
2016-10-10 10:36:20 +0800

    排名&支付fake

--- project/resources/communityPage/js/view.js
+++ project/resources/communityPage/js/view.js
@@ -1,7 +1,7 @@
 var app=angular.module('myApp',[]);
 app.controller("SearchCtrl", function($scope, $http, MyCache) {
-    $scope.url = 'http://anyquant.net:15000/php/serviceController.php'; // The url of our search
-    
+    $scope.url = 'http://115.159.97.98/php/serviceController.php'; // The url of our search
+
         $http.post($scope.url, {
             "method": "getShareStrategyService"
         }).success(function (data, status) {
@@ -44,4 +44,52 @@ app.controller("SearchCtrl", function($scope, $http, MyCache) {
 });
 app.factory('MyCache', function ($cacheFactory) {
     return $cacheFactory('myCache');
-})
+});
+
+$('#gotoStrategyBtn').click(function (e) {
+    $('#competetionBoard').fadeOut(300);
+    $('#strategyBoard').fadeTo(1, 300);
+});
+
+$('#gotoCompetetionBtn').click(function (e) {
+    $('#strategyBoard').fadeOut(300);
+    $('#competetionBoard').fadeTo(1, 300);
+});
+
+var strList=["买入点: kdj黄金交叉点"+"<br/>"+"卖出点: kdj死亡交叉点",
+             "买入点: 上涨0.5%"+"" + "<br/>"+"买入点: 下降0.5%",
+             "买入点: 上涨0.8%"+"<br/>"+"买入点: 下降0.4%",
+             "买入点: kdj&macd黄金交叉点"+"<br/>"+"卖出点: kdj&macd死亡交叉点",
+             "买入点: 上涨0.5%"+"<br/>"+"买入点: 下降0.5%",
+             "买入点: kdj黄金交叉点"+"<br/>"+"卖出点: kdj死亡交叉点",
+             "买入点: 上涨0.5%"+"<br/>"+"买入点: 下降0.5%",
+             "买入点: 上涨0.8%"+"<br/>"+"买入点: 下降0.4%",
+             "买入点: kdj&macd黄金交叉点"+"<br/>"+"卖出点: kdj&macd死亡交叉点",
+             "买入点: 上涨0.5%"+"<br/>"+"买入点: 下降0.5%",
+];
+var selectedBtnID;
+
+$('.insertBtn').click(function(){
+    // 下面这行代码就是你要的ID属性
+    selectedBtnID=$(this).attr("value");
+    var price=10.00-selectedBtnID;
+    conductBuyStrategy(price);
+});
+
+
+function conductBuyStrategy(price){
+    swal({
+        title: "您将支付 "+price+".00 元",
+        text: "您确认付费查看本策略吗?",
+        type: "warning",
+        showCancelButton: true,
+        cancelButtonText: "取消",
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "确定" ,
+        closeOnConfirm: false }, function(){
+        swal("支付成功!", "您可以查看该策略内容!", "success");
+        $('#insertBtn'+selectedBtnID).html(strList[selectedBtnID-1]);
+        $('#insertBtn'+selectedBtnID).css("color","greenyellow");
+    });
+
+}
