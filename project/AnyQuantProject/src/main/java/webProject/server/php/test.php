b19424e42de3ce0638b13ed3fbc28dbbe623bafa
ctg14 <ctg14@software.nju.edu.cn>
2016-10-11 11:19:40 +0800

    test

--- project/AnyQuantProject/src/main/java/webProject/server/php/test.php
+++ project/AnyQuantProject/src/main/java/webProject/server/php/test.php
@@ -5,13 +5,13 @@
  * Date: 2016/10/10
  * Time: 下午9:34
  */
-//     $cmd = "rqalpha run -f examples/28.py -o 28.pkl -s 2016-07-01 -e 2016-09-30 --no-plot";
-//     system($cmd);
-//     $cmd2 = "python pickle2json.py 28.pkl";
-//     system($cmd2,$ret);
-//     $ret = substr($ret,0,-1);
-//     $rankarr = json_decode($ret,true);
-//     echo count($rankObj);
+     $cmd = "rqalpha run -f examples/28.py -o 28.pkl -s 2016-07-01 -e 2016-09-30 --no-plot";
+     exec($cmd);
+     $cmd2 = "python pickle2json.py 28.pkl";
+     exec($cmd2,$ret);
+     $ret = substr($ret,0,-1);
+     $rankarr = json_decode($ret,true);
+     echo count($rankObj);
 
 
 ?>
